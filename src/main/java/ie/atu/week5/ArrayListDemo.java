641a0aee8ae20497498bb5dc60854daa5ef120f1
meeeiiike <g00407095@atu.ie>
2024-10-15 17:04:08 +0100

    user enters new number, gets added to array list. used method "indexOf()" to print current index created for loop added first 10 values from 100 to 1000.
    Tried adding exception handling, works but does not print proper error code, only prints null when trying to get error.

--- src/main/java/ie/atu/week5/ArrayListDemo.java
+++ src/main/java/ie/atu/week5/ArrayListDemo.java
@@ -1,29 +1,45 @@
 package ie.atu.week5;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ArrayListDemo {
 
+    // Prints all elements and shows current index number
     public static void printArrayList(ArrayList<Integer> arrayList) {
         for(Integer i : arrayList){
-            System.out.println(i);
+            System.out.println("Element at index " + arrayList.indexOf(i) + ": " + i);
         }
     }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(100);
-        arrayList.add(200);
-        arrayList.add(300);
-        arrayList.add(400);
-        arrayList.add(500);
-        arrayList.add(600);
-        arrayList.add(700);
-        arrayList.add(800);
-        arrayList.add(900);
-        arrayList.add(1000);
 
+        // Create arrayList and assign 10 values
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        int value = 100;
+
+        // Loop to add 10 values, from 100 incrementing to 1000 instead of manually adding
+        for( int i = 0; i < 10; i++ ){
+            arrayList.add(value);
+            value += 100;
+        }
+        // Print method
+        printArrayList(arrayList);
+
+        // Adding new number to array list
+        System.out.println("Please enter a new number");
+
+        // InputMismatch not working, needed import and still showing null as does "Exception"
+        try{
+            arrayList.add(sc.nextInt());
+
+        }catch(InputMismatchException e){
+            System.out.println("Invalid! Error Message: " + e.getMessage());
+        }
+
+        // Print method with new value added
         printArrayList(arrayList);
     }
 }
